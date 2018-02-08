02a515c4993a26ad3214b6975106298bf8ec9d33
Jc Din <jcdin@channelfix.com>
2018-02-08 18:52:45 +0800

    install django-celery and make celery run

---
+++ README.md
@@ -0,0 +1,4 @@
+##HOW TO RUN CELERY
+```
+celery -A oauth_push_realtime worker -l info
+```
--- oauth_push_realtime/oauth_push_realtime/settings.py
+++ oauth_push_realtime/oauth_push_realtime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import djcelery
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # celery
+    'djcelery',
+    'kombu.transport.django',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# celery config
+# Setup celery module
+djcelery.setup_loader()
+# 
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
+BROKER_URL = 'django://'
